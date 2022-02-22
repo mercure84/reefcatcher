e061156515e5036132954756f37cefc0ee7d5462
Julien M <julien.marcesse@gmail.com>
2022-02-22 21:13:04 +0100

    first commit

---
+++ src/main/java/com/centropygebicolor/reefcatcher/ReefcatcherApplication.java
@@ -0,0 +1,13 @@
+package com.centropygebicolor.reefcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReefcatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReefcatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centropygebicolor/reefcatcher/ReefcatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centropygebicolor.reefcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReefcatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
